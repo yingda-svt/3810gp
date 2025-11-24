7676957817b87d20f92d279e6f737c62442653b1
yingda-svt <carolyan360@gmail.com>
2025-11-24 18:40:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,121 +1,20 @@
 const express = require('express');
 const app = express();
 const fs = require('node:fs/promises');
-const formidable = require('express-formidable'); 
+const formidable = require('express-formidable');
+
 app.use(formidable());
 
-const { MongoClient, ObjectId } = require('mongodb');
-const mongourl = 'mongodb+srv://carolyan360_db_user:01110118@cluster0.55hozbc.mongodb.net/?appName=Cluster0'; // Your MongoDB connection string
-const client = new MongoClient(mongourl);
-const dbName = 'samples_mflix';
-const collectionName = 'comments';
-
+// 设置视图目录
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// Helper functions
-const insertDocument = async (db, doc) => {
-  const collection = db.collection(collectionName);
-  const results = await collection.insertOne(doc);
-  console.log("Inserted document:", results);
-  return results;
-};
-
-const findDocument = async (db, criteria) => {
-  const collection = db.collection(collectionName);
-  console.log(`Finding with criteria: ${JSON.stringify(criteria)}`);
-  const results = await collection.find(criteria).toArray();
-  console.log(`Found ${results.length} documents`);
-  return results;
-};
-
-const updateDocument = async (db, criteria, updateDoc) => {
-  const collection = db.collection(collectionName);
-  const results = await collection.updateOne(criteria, { $set: updateDoc });
-  console.log(`Update results: ${JSON.stringify(results)}`);
-  return results;
-};
-
-// Handlers
-const handle_Create = async (req, res) => {
-  await client.connect();
-  console.log('Connected to MongoDB');
-  const db = client.db(dbName);
-  const newDoc = {
-    bookingid: req.fields.bookingid,
-    mobile: req.fields.mobile
-  };
-
-  if (req.files.filetoupload && req.files.filetoupload.size > 0) {
-    const data = await fs.readFile(req.files.filetoupload.path);
-    newDoc.photo = Buffer.from(data).toString('base64');
-  }
-
-  await insertDocument(db, newDoc);
-  await client.close();
-  console.log('Disconnected from MongoDB');
-  res.redirect('/');
-};
-
-const handle_Find = async (res, criteria) => {
-  await client.connect();
-  console.log('Connected to MongoDB');
-  const db = client.db(dbName);
-  const docs = await findDocument(db, criteria);
-  await client.close();
-  console.log('Disconnected from MongoDB');
-  res.status(200).render('list', { nBookings: docs.length, bookings: docs });
-};
-
-const handle_Details = async (res, criteria) => {
-  await client.connect();
-  console.log('Connected to MongoDB');
-  const db = client.db(dbName);
-  const DOCID = { _id: new ObjectId(criteria._id) };
-  const docs = await findDocument(db, DOCID);
-  await client.close();
-  console.log('Disconnected from MongoDB');
-  res.status(200).render('details', { booking: docs[0] });
-};
-
-const handle_Edit = async (res, criteria) => {
-  await client.connect();
-  console.log('Connected to MongoDB');
-  const db = client.db(dbName);
-  const DOCID = { _id: new ObjectId(criteria._id) };
-  const docs = await findDocument(db, DOCID);
-  await client.close();
-  console.log('Disconnected from MongoDB');
-  res.status(200).render('edit', { booking: docs[0] });
-};
-
-const handle_Update = async (req, res) => {
-  await client.connect();
-  console.log('Connected to MongoDB');
-  const db = client.db(dbName);
-  const DOCID = { _id: new ObjectId(req.fields._id) };
-  const updateDoc = {
-    bookingid: req.fields.bookingid,
-    mobile: req.fields.mobile
-  };
-
-  if (req.files.filetoupload && req.files.filetoupload.size > 0) {
-    const data = await fs.readFile(req.files.filetoupload.path);
-    updateDoc.photo = Buffer.from(data).toString('base64');
-  }
-
-  const results = await updateDocument(db, DOCID, updateDoc);
-  await client.close();
-  console.log('Disconnected from MongoDB');
-  res.status(200).render('info', { message: `Updated ${results.modifiedCount} document(s)` });
-};
-
-// Routes
-app.set('views', __dirname + '/views');
-
+// 根路径，测试服务器是否正常
 app.get('/', (req, res) => {
   res.send('服务器正常运行');
 });
 
+// 测试 /find 路由，渲染 list.ejs
 app.get('/find', (req, res) => {
   const sampleCourses = [
     { _id: '1', course_id: 'CS101', course_name: 'Intro to CS' },
@@ -124,43 +23,36 @@ app.get('/find', (req, res) => {
   res.render('list', { course: sampleCourses });
 });
 
+// 其他路由（示范）
 app.get('/create', (req, res) => {
   res.status(200).render('create', { user: 'demo' });
 });
 
 app.post('/create', (req, res) => {
-  handle_Create(req, res);
+  // 这里你可以调用实际的处理函数
+  res.send('Create post received (示例)');
 });
 
 app.get('/details', (req, res) => {
-  handle_Details(res, req.query);
+  // 这里也写示范
+  res.send('Details page (示例)');
 });
 
 app.get('/edit', (req, res) => {
-  handle_Edit(res, req.query);
+  res.send('Edit page (示例)');
 });
 
 app.post('/update', (req, res) => {
-  handle_Update(req, res);
+  res.send('Update request (示例)');
 });
 
-// Catch-all for unknown routes
+// 404捕获
 app.all('*', (req, res) => {
-  res.status(404).render('info', { message: `${req.path} - Unknown request!` });
+  res.status(404).render('info', { message: `${req.path} - 未找到页面` });
 });
 
-// Start server
+// 启动服务器
 const port = process.env.PORT || 8099;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`服务器监听端口 ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
